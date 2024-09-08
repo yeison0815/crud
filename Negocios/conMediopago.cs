2d0b403481ac87bfa3efe7a1c981200e66ac07c4
Juan José Lezcano <Juanjoselezcanoramirez@outlook.com>
2024-09-08 17:33:55 -0500

    fase 2 de medios de pago listo

--- Negocios/conMediopago.cs
+++ Negocios/conMediopago.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,31 @@ namespace Negocios
 {
     internal class conMediopago
     {
+
+        private modMediopago Pago = new modMediopago();
+        public DataTable MostrarMRod()
+        {
+            DataTable tabla = new DataTable();
+            tabla = Pago.MostrarM();
+            return tabla;
+        }
+        public void InsertarMRod(string nombreC, string descC)
+        {
+            Pago.InsertarM(nombreC, descC);
+        }
+        public void EditarMRod(string nombreC, string descC, string idC)
+        {
+            Pago.EditarM(nombreC, descC, Convert.ToInt32(idC));
+        }
+        public void EliminarMRod(string idc)
+        {
+            Pago.EliminarM(Convert.ToInt32(idc));
+        }
+
+        public string getNombreCategorias(int idC)
+        {
+            return Pago.getNombreMediopago(idC);
+        }
+
     }
 }
